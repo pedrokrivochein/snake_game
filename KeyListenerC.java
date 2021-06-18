1fbb16bb3b3d02842edefcfbfe13d855d32f53d4
Pedro Krivochein <pedroalcantarak@gmail.com>
2021-06-18 08:12:15 -0300

    Update KeyListenerC.java

--- KeyListenerC.java
+++ KeyListenerC.java
@@ -5,9 +5,9 @@ public class KeyListenerC extends KeyAdapter {
     public Player player;
 
     public void keyPressed(KeyEvent evt) {
-        if(evt.getKeyChar() == 'w') player.direction(0, -1);
-        else if(evt.getKeyChar() == 'a') player.direction(-1, 0);
-        else if(evt.getKeyChar() == 's') player.direction(0, 1);
-        else if(evt.getKeyChar() == 'd') player.direction(1, 0);
+        if(evt.getKeyChar() == 'w' || evt.getKeyCode() == 38) player.direction(0, -1);
+        else if(evt.getKeyChar() == 'a' || evt.getKeyCode() == 37) player.direction(-1, 0);
+        else if(evt.getKeyChar() == 's' || evt.getKeyCode() == 40) player.direction(0, 1);
+        else if(evt.getKeyChar() == 'd' || evt.getKeyCode() == 39) player.direction(1, 0);
     }
 }
